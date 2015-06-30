ca321f974a128ec767abfbd19ad90292af96f4b4
Zhaoyu Wei <weixiaobo88@gmail.com>
2015-06-30 11:59:37 +0800

    zy: modify the hard code for constructor

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var Class = (function () {
   function Class(obj) {
-    return function(a, b){
-      this.a = a;
-      this.b = b;
-      this.getA = obj.getA;
-      this.getB = obj.getB;
-    };
+    if(obj.hasOwnProperty('initialize')) {
+      return obj.initialize;
+    }
+    return function() {
+    }
   }
 
   return Class;
